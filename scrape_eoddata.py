3c4f531b9b127b5471b9189dd752e03234643694
John <bagelday@gmail.com>
2015-06-14 12:25:33 -0600

    Third commit: scrapes all letters in alphabet for eoddata.com

--- scrape_eoddata.py
+++ scrape_eoddata.py
@@ -4,37 +4,39 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-#Collect data from website
-address = 'http://eoddata.com/stocklist/NYSE/A.htm'
-
-r = requests.get(address)
-
-soup = BeautifulSoup(r.content)
-
-table = soup.find(id="ctl00_cph1_divSymbols")
-
-headers = table.find('tr')
-
-if 'CodeNameHighLowCloseVolumeChange' not in headers.text:
-	print 'Error detected.'
-	sys.exit()
-
-
-
-#Put data into 2D array
 stockValues = [[] for i in range(7)]
 
-scrapeValue = table.findNext('td')
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-#The first value after the table is "MEMBER LOGIN", so stop when we have scraped that far
-while scrapeValue.text != 'MEMBER LOGIN':
-	for i in range(7):
-		stockValues[i].append(scrapeValue.text)
+for letter in alphabet:
+
+	#Collect data from website
+	address = 'http://eoddata.com/stocklist/NYSE/' + letter + '.htm'
+
+	r = requests.get(address)
+
+	soup = BeautifulSoup(r.content)
+
+	table = soup.find(id="ctl00_cph1_divSymbols")
+
+	headers = table.find('tr')
+
+	if 'CodeNameHighLowCloseVolumeChange' not in headers.text:
+		print 'Error detected.'
+		sys.exit()
+
+	#Put data into 2D array
+	scrapeValue = table.findNext('td')
+
+	#The first value after the table is "MEMBER LOGIN", so stop when we have scraped that far
+	while scrapeValue.text != 'MEMBER LOGIN':
+		for i in range(7):
+			stockValues[i].append(scrapeValue.text)
+			scrapeValue = scrapeValue.findNext('td')
+
 		scrapeValue = scrapeValue.findNext('td')
-
-	scrapeValue = scrapeValue.findNext('td')
-	scrapeValue = scrapeValue.findNext('td')
-	scrapeValue = scrapeValue.findNext('td')
+		scrapeValue = scrapeValue.findNext('td')
+		scrapeValue = scrapeValue.findNext('td')
 
 for i in stockValues:
 	print i
