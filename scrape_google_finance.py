69207aa9ef37af6b3951d15f603d36c76c28b635
John <bagelday@gmail.com>
2015-06-21 22:04:38 -0600

    Made Google Finance scraper get balance sheets for all symbols in database

--- scrape_google_finance.py
+++ scrape_google_finance.py
@@ -8,31 +8,59 @@ from bs4 import BeautifulSoup
 scrapedSheet = []
 sheetList = [[]]
 
-#Scrape data
-address = 'https://www.google.com/finance?fstype=ii&q=nasdaq:aapl'
-r = requests.get(address)
-soup = BeautifulSoup(r.content)
+conn = sqlite3.connect('/home/john/stock_database.db')
+c = conn.cursor()
 
-#The table I'm interested in is the third one with id='fs-table', that's why I find the first one and then findNext two more times
-table = soup.find(id="fs-table").findNext(id='fs-table').findNext(id='fs-table')
+codeList = c.execute('select exchange, code from symbols;')
 
-scrapedSheet.append('AAPL')
-header = table.find('tr').findNext('tr').findNext('td')
+for item in codeList:
+	scrapedSheet = []
+	exchange = str(item[0])
+	code = str(item[1])
+	print exchange
+	print code
+	address = 'https://www.google.com/finance?fstype=ii&q=' + exchange + ':' + code + ''
+	r = requests.get(address)
+	soup = BeautifulSoup(r.content)
+#
+#	#The table I'm interested in is the third one with id='fs-table', that's why I find the first one and then findNext two more times
+	try:
+		table = soup.find(id="fs-table").findNext(id='fs-table').findNext(id='fs-table')
+	except:
+		print 'Table error.'
+		continue
 
-if 'Cash & Equivalents' not in header.text:
-	print 'Error detected, ending script.'
-	sys.exit()
+	scrapedSheet.append(code)
+	header = table.find('tr').findNext('tr').findNext('td')
 
-scrapedValue = header.findNext('td')
-scrapedSheet.append(scrapedValue.text)
-#The last row that needs to be scraped is 'Total Common Shares Outstanding', which is why the loop stops there
-while 'Total Common Shares Outstanding' not in scrapedValue.text:
-	scrapedValue = scrapedValue.findNext('tr')
-	scrapedSheet.append(scrapedValue.findNext('td').findNext('td').text)
+	try:
+		if 'Cash & Equivalents' not in header.text:
+			print 'Error detected, ending script.'
+			sys.exit()
+	except:	
+		print 'Cash & Equivalents not found.'
+		continue
+	
+	scrapedValue = header.findNext('td')
+	scrapedSheet.append(scrapedValue.text)
+	#The last row that needs to be scraped is 'Total Common Shares Outstanding', which is why the loop stops there
+	while 'Total Common Shares Outstanding' not in scrapedValue.text:
+		scrapedValue = scrapedValue.findNext('tr')
+		scrapedSheet.append(scrapedValue.findNext('td').findNext('td').text)
 
-#There should be 43 elements in scrapedSheet, otherwise something went wrong
-if len(scrapedSheet) != 43:
-	print '43 balance sheet elements not found, ending script.'
-	sys.exit()
+	#There should be 43 elements in scrapedSheet, otherwise something went wrong
+	if len(scrapedSheet) != 43:
+#		print '43 balance sheet elements not found, ending script.'
+#		sys.exit()
+		print 'len is not 43'
+		continue
+	
+	print scrapedSheet
+	sheetList.append(scrapedSheet)
+sheetList.pop(0)
 
+for balanceSheet in sheetList:
+	c.execute('insert into balance_sheets values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',balanceSheet)
+
+conn.commit()
 print 'Process complete.'
